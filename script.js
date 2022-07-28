80adf3e0505f70a52913f57e10e528c57634c015
Cozma Rares <cozmarares11@gmail.com>
2022-07-28 22:20:33 +0300

    Finish UI and logic

--- script.js
+++ script.js
@@ -1,53 +1,165 @@
-const choices = ["rock", "paper", "scissors"];
-const numberOfRounds = 5;
-
-function getComputerChoice() {
-  return Math.floor(Math.random() * choices.length);
-}
-
-function drawMessage() {
-  return "It's a draw!";
-}
-
-function winMessage() {
-  return "You win!";
-}
-
-function loseMessage() {
-  return "You lose!";
-}
-
-function playRound(playerSelection, computerSelection) {
-  console.log(`player:${choices[playerSelection]} computer:${choices[computerSelection]}`);
-
-  if (playerSelection === computerSelection) return drawMessage();
-
-  if (Math.abs(playerSelection - computerSelection) === 1)
-    return playerSelection > computerSelection ? winMessage() : loseMessage();
-
-  return playerSelection < computerSelection ? winMessage() : loseMessage();
-}
-
-function getInput() {
-  const choicesString = choices.join(" / ");
-  let input = prompt(`Enter your choice (${choicesString}):`),
-    idx = choices.indexOf(input);
-
-  while (idx === -1) {
-    input = prompt(`Invalid choice, choose again (${choicesString}):`);
-    idx = choices.indexOf(input);
+const messages = {
+  amongus: {
+    loss: "Can't believe a damn stone is stronger than me!!",
+    win: "This week's menu: rabbit stew",
+    sound: "./amongus.mp3"
+  },
+  moai: {
+    loss: "",
+    win: "The might of human is minuscule compared to the ALMIGHTY MOAI",
+    sound: "./boom-sound.mp3"
+  },
+  chungus: {
+    loss: "",
+    win: "",
+    sound: "./chungus.mp3"
+  },
+  impostor: {
+    win: "Computer was the impostor, the RNG gods were not on your side",
+    sound: "./impostor-kill.mp3"
   }
+};
 
-  return idx;
+const pngPaths = {
+  moai: "./moai.png",
+  chungus: "./chungus.png",
+  amongus: "./sus-blue.png",
+  impostor: "./sus-red.png"
+};
+
+const players = ["user", "computer"],
+  USER = 0,
+  COMPUTER = 1;
+
+let userScore = 0,
+  computerScore = 0;
+
+function setFighter(who, shitpost) {
+  document
+    .getElementById(`${who}-fighter`)
+    .setAttribute("src", pngPaths[shitpost]);
 }
 
-function gameLoop() {
-  for (let i = 0; i < numberOfRounds; i++) {
-    const playerSelection = getInput(),
-      computerSelection = getComputerChoice(),
-      message = playRound(playerSelection, computerSelection);
+function setScore(who, score) {
+  document.getElementById(`${who}-score`).innerText = score;
+}
 
-    console.log(message);
-    alert(message);
-  }
+function setWasted(who) {
+  document.getElementById(`${who}-wasted`).style.display = "initial";
+}
+
+function removeWasted() {
+  document.getElementById("user-wasted").style.display = "none";
+  document.getElementById("computer-wasted").style.display = "none";
+}
+
+function addArenaBorderRadius() {
+  const arena = document.getElementById("arena");
+
+  const value = arena.style["border-top-left-radius"];
+
+  arena.style["border-bottom-left-radius"] = value;
+  arena.style["border-bottom-right-radius"] = value;
+}
+
+function removeArenaBorderRadius() {
+  const arena = document.getElementById("arena");
+
+  arena.style["border-bottom-left-radius"] = "0px";
+  arena.style["border-bottom-right-radius"] = "0px";
+}
+
+function displayMessage(message) {
+  removeArenaBorderRadius();
+  const messageBox = document.getElementById("message-box");
+  messageBox.classList.add("active");
+  messageBox.children[0].innerText = message;
+
+  setTimeout(() => {
+    messageBox.classList.remove("active");
+    addArenaBorderRadius();
+  }, 5000);
+}
+
+function getIndex(choice) {
+  return Object.keys(messages).indexOf(choice);
+}
+
+function playSound(source) {
+  const audio = document.getElementsByTagName("audio")[0];
+  audio.src = source;
+  audio.play();
+}
+
+function stopSound() {
+  document.getElementsByTagName("audio")[0].pause();
+}
+
+function updateUI(winner, score, message, sound) {
+  setScore(players[winner], score);
+  setWasted(players[1 - winner]);
+  playSound(sound);
+  displayMessage(message);
+  document.body.style.pointerEvents = "none";
+}
+
+document.getElementsByTagName("audio")[0].addEventListener("ended", () => {
+  document.body.style.pointerEvents = "initial";
+});
+
+function playRound(userSelection) {
+  removeWasted();
+
+  const computerSelection =
+    Math.random() > 0.95
+      ? "impostor" // 5% chance to get impostor
+      : Object.keys(messages)[Math.floor(Math.random() * 3)];
+
+  setFighter("user", userSelection);
+  setFighter("computer", computerSelection);
+
+  if (computerSelection === "impostor")
+    return updateUI(
+      COMPUTER,
+      ++computerScore,
+      messages.impostor.win,
+      messages.impostor.sound
+    );
+
+  const userSelectionIndex = getIndex(userSelection),
+    computerSelectionIndex = getIndex(computerSelection);
+
+  if (userSelectionIndex === computerSelectionIndex)
+    return displayMessage("It's a tie!");
+
+  if (Math.abs(userSelectionIndex - computerSelectionIndex) === 1)
+    if (userSelectionIndex > computerSelectionIndex)
+      return updateUI(
+        USER,
+        ++userScore,
+        messages[userSelection].win,
+        messages[userSelection].sound
+      );
+    else
+      return updateUI(
+        COMPUTER,
+        ++computerScore,
+        messages[userSelection].loss,
+        messages[computerSelection].sound
+      );
+
+  if (userSelectionIndex < computerSelectionIndex)
+    return updateUI(
+      USER,
+      ++userScore,
+      messages[userSelection].win,
+      messages[userSelection].sound
+    );
+  else
+    return updateUI(
+      COMPUTER,
+      ++computerScore,
+      messages[userSelection].loss,
+      messages[computerSelection].sound
+    );
 }
